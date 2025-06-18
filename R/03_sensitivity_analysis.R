8d2714fc455d0c85bd5fe5f66cba90ab5f79c4ac
macataci <mc.tavera@uniandes.edu.co>
2025-06-17 19:31:19 -0500

    fixed typo in dsa values and its associated graphs

--- R/03_sensitivity_analysis.R
+++ R/03_sensitivity_analysis.R
@@ -1,11 +1,11 @@
 
 sa <- define_dsa(
-  qlC1, .108, .123,
-  qlC2, .45, .55,
-  qlS1, .108, .123,
+  qlC1, 0.108, 0.132,
+  qlC2, 0.45, 0.55,
+  qlS1, 0.108, 0.132,
   qlS2, 0.036, 0.044,
   pCM1, 0.027, 0.033,
-  pCM2, 0.135, 0.165
+  pCM2, 0.135, 0.165,
 )
 
 res_dsa <- run_dsa(
@@ -13,29 +13,29 @@ res_dsa <- run_dsa(
   dsa = sa
 )
 
-png("figs/03_dsa_treat1.png", width = 650, height = 400)
+# png("figs/03_dsa_treat1.png", width = 650, height = 400)
 plot(res_dsa,
      strategy = "tratamiento_1",
      result = "effect",
      type = "simple") + xlab("Calidad de Vida") +
   ggtitle("Resultado Análisis de Sensibilidad para el Tratamiento 1")+
   theme(plot.title = element_text(hjust=0.5), plot.margin = margin(1, 1, 1, 1, "cm"))
-dev.off()
+# dev.off()
 
-png("figs/03_dsa_treat2.png", width = 650, height = 400)
+# png("figs/03_dsa_treat2.png", width = 650, height = 400)
 plot(res_dsa,
      strategy = "tratamiento_2",
      result = "effect",
      type = "simple") + xlab("Calidad de Vida")+
   ggtitle("Resultado Análisis de Sensibilidad para el Tratamiento 2")+
   theme(plot.title = element_text(hjust=0.5), plot.margin = margin(1, 1, 1, 1, "cm"))
-dev.off()
+# dev.off()
 
-png("figs/03_dsa_diff.png", width = 650, height = 400)
+# png("figs/03_dsa_diff.png", width = 650, height = 400)
 plot(res_dsa,
      strategy = "tratamiento_1",
      result = "effect",
      type = "difference")+ xlab("Calidad de Vida") +
-  ggtitle("Resultado Análisis de Sensibilidad Respecto al Valor Base")+
+  ggtitle("Resultado Análisis de Sensibilidad para Diferencia entre Tratamientos")+
   theme(plot.title = element_text(hjust=0.5), plot.margin = margin(1, 1, 1, 1, "cm"))
-dev.off()
+# dev.off()
